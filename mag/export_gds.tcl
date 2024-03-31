5ce0a031bdb159478c1a3cd6c131ba59c2051245
Uri Shaked <uri@urishaked.com>
2024-03-31 11:46:31 +0300

    feat(layout): WiP layout
    should be tiny-tapeout submittable, but not functional yet

---
+++ mag/export_gds.tcl
@@ -0,0 +1,2 @@
+gds write ../gds/tt_um_urish_charge_pump.gds
+lef write ../lef/tt_um_urish_charge_pump.lef -pinonly
---
+++ mag/init_layout.tcl
@@ -0,0 +1,29 @@
+# Important: before running this script, download the the .def file from
+# https://github.com/TinyTapeout/tt-support-tools/blob/tt06/def/analog/tt_block_1x2_pg_ana.def
+
+# Load schematic from spice
+magic::netlist_to_layout "~/.xschem/simulations/dickson.spice" sky130
+
+def read tt_block_1x2_pg_ana.def
+cellname rename tt_um_template tt_um_urish_charge_pump
+
+# VPWR
+box 100 500 250 22076
+paint met4
+label VPWR FreeSans met4
+port VPWR make n
+port VPWR use power
+port VPWR class bidirectional
+port conn n s e w
+
+# VGND
+box 4900 500 5050 22076
+paint met4
+label VGND FreeSans met4
+port VGND make n
+port VGND use ground
+port VGND class bidirectional
+port conn n s e w
+
+grid 0.5um 0.5um
+snap user
